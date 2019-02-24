26305064cebe92f42aafe09c364fae573de5cd7d
Lukas Herman <lherman.cs@gmail.com>
2019-02-24 12:37:05 -0500

    assignment-3: finished 5

--- assignment-3/tracker/ioMod.cpp
+++ assignment-3/tracker/ioMod.cpp
@@ -60,3 +60,18 @@ void IoMod::writeText(const std::string& msg, int x, int y) const {
   SDL_RenderCopy(renderer, texture, NULL, &dst);
   SDL_DestroyTexture(texture);
 }
+
+void IoMod::writeText(const std::string& msg, int x, int y,
+                      SDL_Color color) const {
+  SDL_Surface* surface = TTF_RenderText_Solid(font, msg.c_str(), color);
+
+  SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+
+  int textWidth = surface->w;
+  int textHeight = surface->h;
+  SDL_FreeSurface(surface);
+  SDL_Rect dst = {x, y, textWidth, textHeight};
+
+  SDL_RenderCopy(renderer, texture, NULL, &dst);
+  SDL_DestroyTexture(texture);
+}
--- assignment-3/tracker/ioMod.h
+++ assignment-3/tracker/ioMod.h
@@ -1,19 +1,21 @@
-#include <iostream>
-#include <string>
 #include <SDL.h>
 #include <SDL_ttf.h>
+#include <iostream>
+#include <string>
 
 class IoMod {
-public:
+ public:
   static IoMod& getInstance();
   ~IoMod();
   SDL_Texture* readTexture(const std::string& filename);
   SDL_Surface* readSurface(const std::string& filename);
   void writeText(const std::string&, int, int) const;
+  void writeText(const std::string& msg, int x, int y, SDL_Color color) const;
   SDL_Renderer* getRenderer() const { return renderer; }
   IoMod(const IoMod&) = delete;
   IoMod& operator=(const IoMod&) = delete;
-private:
+
+ private:
   int init;
   SDL_Renderer* renderer;
   TTF_Font* font;
