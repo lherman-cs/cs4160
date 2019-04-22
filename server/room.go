6abe9ddaee5e0c98e9196c1be72e2cffc37db1d3
Lukas Herman <lherman.cs@gmail.com>
2019-04-22 01:05:45 -0400

    Added detailRoom and added task helper script

--- server/room.go
+++ server/room.go
@@ -1,14 +1,25 @@
 package main
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
 type room struct {
 	name    string
 	players []net.Conn
+	m       sync.Mutex
 }
 
 func newRoom(name string) *room {
 	return &room{
 		name: name,
 	}
+}
+
+func (r *room) join(conn net.Conn) {
+	r.m.Lock()
+	defer r.m.Unlock()
+
+	r.players = append(r.players, conn)
 }
--- server/router.go
+++ server/router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net"
+	"strconv"
 
 	"github.com/google/uuid"
 
@@ -24,18 +25,23 @@ func handle(conn net.Conn) {
 		log.Error("[router] command doesn't exist in message")
 		return
 	}
+	log.Debug("[router] received command ", cmd)
 
 	switch cmd {
 	case "create-room":
 		createRoom(conn, msg)
 	case "list-rooms":
 		listRooms(conn)
+	case "detail-room":
+		detailRoom(conn, msg)
 	default:
 		log.Error("[router] received invalid command")
 		return
 	}
 }
 
+// requires:
+//	- name: room's name
 func createRoom(conn net.Conn, msg map[string]string) {
 	log.Info("[router] handle createRoom command")
 
@@ -69,6 +75,39 @@ func listRooms(conn net.Conn) {
 	})
 
 	err := newEncoder(conn).encode(msg)
+	if err != nil {
+		log.Error("[router] failed in encoding message")
+		return
+	}
+
+	log.Debug("[router] ", msg)
+}
+
+// requires:
+//	- id: room's id
+// response:
+//	- name: room's name
+//	- num_players: the number of players who have joined
+func detailRoom(conn net.Conn, msg map[string]string) {
+	log.Info("[router] handle detailRoom command")
+
+	id, ok := msg["id"]
+	if !ok {
+		log.Error("[router] id is required")
+		return
+	}
+
+	value, ok := rooms.Load(id)
+	if !ok {
+		log.Error("[router] room doesn't exist")
+		return
+	}
+	room := value.(*room)
+	resp := make(map[string]string)
+	resp["name"] = room.name
+	resp["num_players"] = strconv.Itoa(len(room.players))
+
+	err := newEncoder(conn).encode(resp)
 	if err != nil {
 		log.Error("[router] failed in encoding message")
 		return
