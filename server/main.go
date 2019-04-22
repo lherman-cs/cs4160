b63810f8f0142c4c485cfa4c9bfcf6b1b495dc20
Lukas <lherman.cs@gmail.com>
2019-04-22 09:44:14 -0400

    Added websocket support

--- server/main.go
+++ server/main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"net"
+	"net/http"
 	"os"
+
+	"golang.org/x/net/websocket"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	log.SetLevel(log.DebugLevel)
-	list, err := net.Listen("tcp", ":8080")
+const (
+	tcpAddr = ":8081"
+	wsAddr  = ":8080"
+)
+
+func startTCPService() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error("[tcp] service exited with panic, restarting...")
+		}
+	}()
+
+	list, err := net.Listen("tcp", tcpAddr)
 	if err != nil {
-		log.Error("[main]", err)
+		log.Error("[tcp]", err)
 		os.Exit(1)
 	}
 	defer list.Close()
@@ -19,10 +32,40 @@ func main() {
 	for {
 		client, err := list.Accept()
 		if err != nil {
-			log.Warn("[main] ", err)
+			log.Warn("[tcp] ", err)
 			continue
 		}
-		log.Info("[main] received a connection from ", client.RemoteAddr().String())
+		log.Info("[tcp] received a connection from ", client.RemoteAddr().String())
 		go handle(client)
+	}
+}
+
+func startWSService() {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Error("[ws] service exited with panic, restarting...")
+		}
+	}()
+
+	http.Handle("/", websocket.Handler(func(ws *websocket.Conn) {
+		log.Info("[ws] received a connection from ", ws.RemoteAddr().String())
+		go handle(ws)
+	}))
+	err := http.ListenAndServe(wsAddr, nil)
+	if err != nil {
+		log.Error("[ws] websocket service failed")
+	}
+}
+
+func main() {
+	log.SetLevel(log.DebugLevel)
+	go func() {
+		for {
+			startTCPService()
+		}
+	}()
+
+	for {
+		startWSService()
 	}
 }
