e13a23f93d6d66628ad3681723161b7c6eda911a
Lukas <lherman.cs@gmail.com>
2019-04-24 18:03:27 -0400

    Added wasm interopbility

--- tcp/main.cpp
+++ tcp/main.cpp
@@ -4,7 +4,11 @@
 #include "tcp.h"
 
 int main() {
+#ifdef __EMSCRIPTEN__
+  TCP tcp("127.0.0.1:8080");
+#else
   TCP tcp("127.0.0.1:8081");
+#endif
   std::unordered_map<std::string, std::string> in;
   std::unordered_map<std::string, std::string> out;
   out["command"] = "create";
