e0a21ff000c1566ac8a10618a73ad87bbbc8fc8c
Lukas <lherman.cs@gmail.com>
2019-04-26 13:11:27 -0400

    Added reflect tags for encoding

--- server/encoding.go
+++ server/encoding.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/spf13/cast"
 	"io"
+	"reflect"
 	"strings"
 )
 
@@ -20,14 +22,52 @@ func newEncoder(w io.Writer) *encoder {
 	return &encoder{w: bufio.NewWriter(w)}
 }
 
-// encode formats a map and returns a string
-func (e *encoder) encode(table map[string]string) error {
-	for k, v := range table {
-		_, err := e.w.WriteString(k + msgeq + v + msgsep)
-		if err != nil {
-			return err
+func (e *encoder) toString(v interface{}) string {
+	value := reflect.ValueOf(v)
+	kind := value.Kind()
+	switch kind {
+	case reflect.Slice:
+		slice := make([]string, 0, value.Len())
+		for i := 0; i < value.Len(); i++ {
+			slice = append(slice, cast.ToString(value.Index(i).Interface()))
 		}
+		return strings.Join(slice, ",")
+	default:
+		return cast.ToString(value.Interface())
 	}
+}
+
+// encode formats a map and returns a string
+func (e *encoder) encode(v interface{}) error {
+	value := reflect.ValueOf(v)
+	kind := value.Kind()
+
+	switch kind {
+	case reflect.Map:
+		iter := value.MapRange()
+		for iter.Next() {
+			k := iter.Key().String()
+			v := iter.Value().String()
+			_, err := e.w.WriteString(k + msgeq + v + msgsep)
+			if err != nil {
+				return err
+			}
+		}
+	case reflect.Struct:
+		st := reflect.TypeOf(v)
+		for i := 0; i < st.NumField(); i++ {
+			typeField := st.Field(i)
+			alias, ok := typeField.Tag.Lookup("msg")
+			if !ok {
+				continue
+			}
+			valueStr := e.toString(value.Field(i).Interface())
+			e.w.WriteString(alias + msgeq + valueStr + msgsep)
+		}
+	default:
+		return fmt.Errorf("doesn't support %s", kind.String())
+	}
+
 	e.w.WriteRune('\n')
 	return e.w.Flush()
 }
--- server/encoding_test.go
+++ server/encoding_test.go
@@ -25,6 +25,27 @@ func TestEncode(t *testing.T) {
 	}
 }
 
+func TestEncodeReflect(t *testing.T) {
+	table := struct {
+		Players []string `msg:"name"`
+		Ages    []int    `msg:"age"`
+	}{Players: []string{"Lukas", "Poop"}, Ages: []int{23, 30}}
+
+	var b bytes.Buffer
+
+	err := newEncoder(&b).encode(table)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected1 := "age:23,30\tname:Lukas,Poop\t\n"
+	expected2 := "name:Lukas,Poop\tage:23,30\t\n"
+	got := b.String()
+	if !(got == expected1 || got == expected2) {
+		t.Error("expected1:", expected1, "expected2:", expected2, "got:", got)
+	}
+}
+
 func TestDecode(t *testing.T) {
 	encoded := "name:Lukas\tage:23\t"
 	var b bytes.Buffer
