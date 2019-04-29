6904171971444513231b1eec4fa792e2ad0fdeda
Lukas Herman <lherman.cs@gmail.com>
2019-04-28 22:45:57 -0400

    Added bot logic implementation

--- server/bot.go
+++ server/bot.go
@@ -5,6 +5,9 @@ import (
 	"github.com/Pallinder/go-randomdata"
 	"github.com/sirupsen/logrus"
 	"io"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
 type bot struct {
@@ -36,5 +39,75 @@ func newBotFromHuman(h *human) *bot {
 }
 
 func (b *bot) Loop() {
+	maxDelay := 10 // seconds
+	minDelay := 3
 
+	for {
+		msg := make(map[string]string)
+		newDecoder(b).decode(msg)
+
+		t := msg["type"]
+		if t != "state" {
+			continue
+		}
+
+		turn, _ := strconv.ParseInt(msg["turn"], 10, 64)
+		numDice, _ := strconv.ParseInt(msg["num_dices"], 10, 64)
+		lastQuantity, _ := strconv.ParseInt(msg["last_quantity"], 10, 64)
+		lastFace, _ := strconv.ParseInt(msg["last_face"], 10, 64)
+
+		called := b.maybeCall(numDice, lastQuantity, lastFace)
+		if called {
+			continue
+		}
+
+		if turn != int64(b.id) {
+			continue
+		}
+
+		delay := rand.Intn(maxDelay) + minDelay
+		if delay > maxDelay {
+			delay = maxDelay
+		}
+		time.Sleep(time.Duration(delay) * time.Second)
+		b.bet(numDice, lastQuantity, lastFace)
+	}
+}
+
+func (b *bot) maybeCall(numDice, lastQuantity, lastFace int64) bool {
+	var match int
+
+	quantity := int(lastQuantity)
+	face := int(lastFace)
+
+	for _, die := range b.dice {
+		if face == die {
+			match++
+		}
+	}
+
+	// guaranteed it's not a lie
+	if quantity <= match {
+		return false
+	}
+
+	// probability with what we have and absolute 15% chance to always call
+	if rand.Intn(quantity) < match || rand.Intn(100) < 15 || quantity == int(numDice) {
+		b.g.mailbox <- eventCall{&event{b}}
+		return true
+	}
+	return false
+}
+
+func (b *bot) bet(numDice, lastQuantity, lastFace int64) {
+	quantity := int(lastQuantity)
+	face := int(lastFace)
+
+	if rand.Intn(100) < 80 || face == 5 {
+		quantity++
+	} else {
+		face++
+	}
+
+	b.g.mailbox <- eventBet{&event{b}, quantity, face}
 }
