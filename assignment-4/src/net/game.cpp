a4bf0533b5d618e26bfa472ba3029414dd2b4082
Gabrielle Stewart <gestewa@clemson.edu>
2019-04-28 20:37:01 -0400

    fleshed out networked game initalization

--- assignment-4/src/net/game.cpp
+++ assignment-4/src/net/game.cpp
@@ -29,5 +29,90 @@ NetGameScreen::NetGameScreen(int difficulty) {
 NetGameScreen::~NetGameScreen() {}
 
 void NetGameScreen::onKeyDown(const Uint8* const keystate) { (void)keystate; }
+
 void NetGameScreen::draw() const {}
-void NetGameScreen::update(Uint32 ticks) {}
+
+void NetGameScreen::update(Uint32 ticks) {
+  // capture offline
+  if (session->isOffline()) return;
+  if (state == Initalizing) {
+    // Create appropriate data structures
+
+    auto& promise = Global::get().promise.add();
+    auto resp = std::make_shared<net::message>();
+
+    // Get dice
+
+    auto getDice = [=]() {
+      if (session->isOffline()) {
+        joining = false;
+        return true;
+      }
+      bool done = session->read(*resp);
+      if (!done) return false;
+
+      if (resp->find("error") != resp->end() ||
+          resp->find("roll") != resp->end()) {
+        // joining = false;
+        return true;
+      }
+
+      auto& navigator = Global::get().navigator;
+      navigator.pop();
+      navigator.push<RoomScreen>(session, 0, false);
+      return true;
+    };
+
+    promise.then(getDice);
+
+    for (auto player : gameData->players) {
+    }
+
+    // Get state
+
+    auto getState = [=]() {
+      if (session->isOffline()) {
+        joining = false;
+        return true;
+      }
+      bool done = session->read(*resp);
+      if (!done) return false;
+
+      if (resp->find("error") != resp->end()) {
+        joining = false;
+        return true;
+      }
+
+      auto& navigator = Global::get().navigator;
+      navigator.pop();
+      navigator.push<RoomScreen>(session, 0, false);
+      return true;
+    };
+
+    promise.then(getState);
+
+    // Check if done
+    state = Ongoing;
+    return;
+  }
+
+  // Handle player going offline
+  if (session->isOffline()) {
+    auto loading = Global::get().widget.create<Loading>("You're offline");
+    auto redirecting = Global::get().widget.create<Loading>("Going back");
+    auto goBack = []() {
+      Global::get().navigator.pop();
+      return true;
+    };
+
+    auto& promise = Global::get().promise.add();
+    promise.then(loading->show())
+        .sleep(2000)
+        .then(loading->dismiss())
+        .then(redirecting->show())
+        .sleep(500)
+        .then(goBack)
+        .sleep(500)
+        .then(redirecting->dismiss());
+  }
+}
