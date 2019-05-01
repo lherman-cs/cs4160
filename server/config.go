fd47b6d9c0351aaa0f9f5637ddb8ff3684ab2eec
Lukas Herman <lherman.cs@gmail.com>
2019-05-01 01:08:41 -0400

    Changed default max delay for bots to be 2s

--- server/config.go
+++ server/config.go
@@ -12,7 +12,7 @@ var (
 	delayShowDice  = time.Duration(getEnvInt("DELAY_SHOW_DICE", 0)) * time.Millisecond
 	delayAfterCall = time.Duration(getEnvInt("DELAY_AFTER_CALL", 0)) * time.Millisecond
 	delayBotMin    = getEnvInt("DELAY_BOT_MIN", 1000)
-	delayBotMax    = getEnvInt("DELAY_BOT_MAX", 3000)
+	delayBotMax    = getEnvInt("DELAY_BOT_MAX", 2000)
 )
 
 func getEnv(key, fallback string) string {
